b2ef7314b5d30b825169b43fdde0bb20e65c04b3
Rodrigo Vasconcelos <rodrigo.vasconcelos@ifactory.com.br>
2017-06-01 12:50:39 -0300

    ExchangeRate.at spike

--- lib/exchange_rate.rb
+++ lib/exchange_rate.rb
@@ -3,21 +3,25 @@ require 'nokogiri'
 
 module ExchangeRate
   
-  EXCHANGE_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
+  ECB_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
+  @@ECB_DATA = nil
 
-  def ExchangeRate.at(time,from,to)
-    "#{time},#{from},#{to}"
+  def ExchangeRate.at(timestamp,from,to)
+    from_value = ExchangeRate.rate_for_currency(timestamp,from)
+    to_value = ExchangeRate.rate_for_currency(timestamp,to)
+    to_value/from_value
   end
 
 
   def ExchangeRate.read_exchange_data    
-    Nokogiri::XML(open(EXCHANGE_URL).read).remove_namespaces!
+    @@ECB_DATA ||= Nokogiri::XML(open(ECB_URL).read).remove_namespaces!
   end
 
   def ExchangeRate.rate_for_currency(timestamp,currency)
   	ExchangeRate
   		.read_exchange_data
   		.xpath("//Cube/Cube[@time = '#{timestamp}']/Cube[@currency = '#{currency}']/@rate")
+  		.first.value.to_f
   end
 
 end
