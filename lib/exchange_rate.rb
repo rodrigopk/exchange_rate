2f8de619f46f03380aab31cce2d5a003567745aa
Rodrigo Vasconcelos <rodrigo.vasconcelos@ifactory.com.br>
2017-06-02 09:29:59 -0300

    cache spike

--- lib/exchange_rate.rb
+++ lib/exchange_rate.rb
@@ -2,26 +2,69 @@ require 'open-uri'
 require 'nokogiri'
 
 module ExchangeRate
-  
-  ECB_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
-  @@ECB_DATA = nil
 
-  def ExchangeRate.at(timestamp,from,to)
-    from_value = ExchangeRate.rate_for_currency(timestamp,from)
-    to_value = ExchangeRate.rate_for_currency(timestamp,to)
+  @@ecb_url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
+  @@cache_uri = Rails.public_path.to_s+"/rates.xml"
+  @@cache = nil
+
+  def self.set_cache_uri(path)
+    @@cache_uri = path   
+    update_cache
+  end
+
+  def self.cache_uri
+    @@cache_uri  
+  end
+
+  def self.set_cache(cache)
+    @@cache = cache
+  end
+
+  def self.cache
+    @@cache
+  end
+
+  def self.set_url(url)
+    @@ecb_url = url    
+  end
+
+  def self.url
+    @@ecb_url
+  end
+
+  def self.fetch_data
+    unless File.exist?(cache_uri)
+      self.save_data(cache_uri)
+    end
+      Nokogiri::XML(File.open(cache_uri).read)
+  end
+
+  def self.update_cache
+    set_cache(Nokogiri::XML(File.open(cache_uri).read))
+  end
+
+  def self.save_data(path)
+    File.open(path, "w") do |file|
+      ecb_data = open(url);
+      ecb_data.each_line do |line| 
+        file.puts line
+      end
+    end
+    update_cache
+  end
+
+  def self.at(time,from,to)
+    timestamp = time.to_s
+    from_value = rate_for_currency(timestamp,from)
+    to_value = rate_for_currency(timestamp,to)
     to_value/from_value
   end
 
-
-  def ExchangeRate.read_exchange_data    
-    @@ECB_DATA ||= Nokogiri::XML(open(ECB_URL).read)
+  def self.rate_for_currency(timestamp,currency)
+    cache
+      .xpath("gesmes:Envelope/xmlns:Cube/xmlns:Cube[@time = '#{timestamp}']/xmlns:Cube[@currency = '#{currency}']/@rate")
+      .first.value.to_f
   end
 
-  def ExchangeRate.rate_for_currency(timestamp,currency)
-  	ExchangeRate
-  		.read_exchange_data
-  		.xpath("gesmes:Envelope/xmlns:Cube/xmlns:Cube[@time = '#{timestamp}']/xmlns:Cube[@currency = '#{currency}']/@rate")
-  		.first.value.to_f
-  end
-
+  set_cache(fetch_data)
 end
--- test/exchange_rate_test.rb
+++ test/exchange_rate_test.rb
@@ -2,8 +2,21 @@ require 'test_helper'
 
 class ExchangeRate::Test < ActiveSupport::TestCase  
 
-  def test_at_returns_value    
-    assert_equal "#{Date.today},GBP,USD", ExchangeRate.at(Date.today,'GBP','USD')
+	def setup
+		ExchangeRate.set_cache_uri(Dir.pwd+'/test/fixtures/test_data.xml')
+		@date = Date.new(2017,6,2)
+	end
+
+  def test_data_cached_in_memory_value    
+    assert_not ExchangeRate.cache.nil?
+  end
+
+  def test_rate_for_currency_return    
+    assert_equal 1.2853508731722967, ExchangeRate.at(@date,'GBP','USD')
+  end
+
+  def test_at_return    
+    assert_equal 1.1217, ExchangeRate.rate_for_currency(@date.to_s,'USD')
   end
 
 end
